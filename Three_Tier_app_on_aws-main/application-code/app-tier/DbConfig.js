bff1da78900307023b71bc762282c5a1f14b7147
praneeth <praneethmikkilineni886@gmail.com>
2025-01-16 12:51:12 -0600

    new project

---
+++ Three_Tier_app_on_aws-main/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  DB_HOST: "rds-three-tier-instance-1.c1y6m442usdl.us-east-1.rds.amazonaws.com",
+  DB_USER: "admin",
+  DB_PWD: "praneeth12345",
+  DB_DATABASE: "webappdb",
+});
